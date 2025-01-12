12367ad3be46d9ddd2ac03674f5a7d9f2be77cab
우승식 <intelryzen@naver.com>
2025-01-12 09:24:58 +0900

    fix: Incorrect Locale Error Handling

--- lib/birth_picker.dart
+++ lib/birth_picker.dart
@@ -24,7 +24,7 @@ class BirthPicker extends StatefulWidget {
   final Widget? icon;
   final Color? iconColor;
   final double iconSize;
-  final Function(DateTime? dateTime)? onChanged;
+  final void Function(DateTime? dateTime)? onChanged;
 
   const BirthPicker({
     super.key,
@@ -67,6 +67,216 @@ class _BirthPickerState extends State<BirthPicker> {
 
   final keyboardEventFocusNode = FocusNode();
   DateTime? currentDate;
+
+  @override
+  void initState() {
+    super.initState();
+    hintTexts = getLocalizedDateUnits(widget.locale ?? Platform.localeName);
+    _setFieldOrder();
+    _addFocusListeners();
+    _addControllerListeners();
+  }
+
+  Map<String, String> getLocalizedDateUnits(String locale) {
+    List<String> dateFormat = BirthPickerLocalization.getDateFormat(locale);
+
+    return {
+      'year': dateFormat[0],
+      'month': dateFormat[1],
+      'day': dateFormat[2],
+    };
+  }
+
+  void _setFieldOrder() {
+    try {
+      String? pattern =
+          DateFormat.yMd(widget.locale ?? Platform.localeName).pattern;
+      if (pattern != null) {
+        fieldOrder = _extractFieldOrder(pattern);
+      }
+    } catch (_) {}
+  }
+
+  List<String> _extractFieldOrder(String pattern) {
+    List<MapEntry<String, int>> fieldsWithPosition = [
+      MapEntry('year', pattern.indexOf('y')),
+      MapEntry('month', pattern.indexOf('M')),
+      MapEntry('day', pattern.indexOf('d')),
+    ]..sort((a, b) => a.value.compareTo(b.value));
+
+    return fieldsWithPosition.map((entry) => entry.key).toList();
+  }
+
+  void _addFocusListeners() {
+    for (var entry in focusNodes.entries) {
+      entry.value.addListener(() {
+        String fieldType = entry.key;
+        String text = fieldTexts[fieldType]!;
+
+        // Automatically checks the format of text when focus is released
+        if (!entry.value.hasFocus && text.isNotEmpty) {
+          final maxLength = (fieldType == 'year') ? 4 : 2;
+          var (correctedValue, _) = BirthPickerUtil.checkDateFormat(
+              fieldType, text.padLeft(maxLength, "0"));
+          fieldTexts[fieldType] = correctedValue;
+          controllers[fieldType]!.text = correctedValue;
+        }
+
+        setState(() {});
+      });
+    }
+  }
+
+  void _addControllerListeners() {
+    for (var controller in controllers.values) {
+      controller.addListener(() async {
+        // Keep the cursor always at the end of the text
+        if (controller.selection ==
+            TextSelection.fromPosition(
+                TextPosition(offset: controller.text.length - 1))) {
+          controller.selection = TextSelection.fromPosition(
+            TextPosition(
+              offset: controller.text.length,
+            ),
+          );
+        }
+
+        if (widget.onChanged != null) {
+          await Future.delayed(Duration.zero);
+          final date = _getDate();
+
+          if (currentDate != date) {
+            currentDate = date;
+            widget.onChanged!.call(date);
+          }
+        }
+      });
+    }
+  }
+
+  DateTime? _getDate() {
+    int? year = int.tryParse(fieldTexts["year"]!);
+    int? month = int.tryParse(fieldTexts['month']!);
+    int? day = int.tryParse(fieldTexts['day']!);
+    bool isValidDate = false;
+
+    if (year != null && month != null && day != null) {
+      isValidDate = BirthPickerUtil.isValidDate(year, month, day);
+    }
+
+    return isValidDate ? DateTime(year!, month!, day!) : null;
+  }
+
+  void _handleKeyEvent(KeyEvent event) {
+    // Get the currently focused field.
+    final focusedField = focusNodes.entries
+        .firstWhere((entry) => entry.value.hasFocus,
+            orElse: () => MapEntry('', FocusNode()))
+        .key;
+
+    if (focusedField.isEmpty) return;
+
+    final currentIndex = fieldOrder.indexOf(focusedField);
+
+    if (event.logicalKey == LogicalKeyboardKey.arrowLeft && currentIndex > 0) {
+      _requestFocus(fieldOrder[currentIndex - 1]);
+    } else if (event.logicalKey == LogicalKeyboardKey.arrowRight &&
+        currentIndex < fieldOrder.length - 1) {
+      _requestFocus(fieldOrder[currentIndex + 1]);
+    }
+  }
+
+  Widget _buildField(BuildContext context, String fieldType) {
+    final controller = controllers[fieldType]!;
+    final focusNode = focusNodes[fieldType]!;
+    final hintText = hintTexts[fieldType]!;
+    final maxLength = (fieldType == 'year') ? 4 : 2;
+    final defaultTextStyle = widget.textStyle ?? TextStyle(fontSize: 16);
+    final text = fieldTexts[fieldType]!;
+
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => _requestFocus(fieldType),
+      child: Stack(
+        children: [
+          Center(
+            child: Row(
+              children: [
+                Container(
+                  padding: widget.focusPadding ??
+                      const EdgeInsets.symmetric(horizontal: 2, vertical: 2),
+                  decoration: BoxDecoration(
+                    color: focusNode.hasFocus
+                        ? widget.focusColor ??
+                            (Theme.of(context).brightness == Brightness.dark
+                                ? const Color(0xff2176FF).withValues(alpha: 0.7)
+                                : const Color(0xffb4d7ff))
+                        : null,
+                  ),
+                  child: Text(
+                    text.isEmpty ? hintText : text.padLeft(maxLength, '0'),
+                    style: defaultTextStyle,
+                  ),
+                ),
+                Text(
+                  ".",
+                  style: defaultTextStyle,
+                ),
+              ],
+            ),
+          ),
+          IgnorePointer(
+            ignoring: true,
+            child: Center(
+              child: SizedBox(
+                width: 0,
+                height: 0,
+                child: TextField(
+                  controller: controller,
+                  focusNode: focusNode,
+                  keyboardType: TextInputType.number,
+                  enableInteractiveSelection: false,
+                  enableSuggestions: false,
+                  autofocus: widget.autofocus && fieldType == fieldOrder.first,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    counterText: '',
+                    border: UnderlineInputBorder(borderSide: BorderSide.none),
+                  ),
+                  onChanged: (value) {
+                    if (text.length > value.length) {
+                      fieldTexts[fieldType] = "";
+                      controller.clear();
+                    } else {
+                      if (value.length > maxLength) {
+                        value = value.substring(maxLength, value.length);
+                      }
+
+                      if (BirthPickerUtil.isInteger(value)) {
+                        // 숫자일 경우 실행할 코드
+                        var (correctedValue, moveNext) =
+                            BirthPickerUtil.checkDateFormat(fieldType, value);
+
+                        fieldTexts[fieldType] = correctedValue;
+
+                        if (moveNext) {
+                          _moveFocusToNextField(fieldType);
+                        }
+                        controller.text = correctedValue;
+                      } else {
+                        controller.text = text;
+                      }
+                    }
+                    setState(() {});
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -183,19 +393,6 @@ class _BirthPickerState extends State<BirthPicker> {
     );
   }
 
-  DateTime? _getDate() {
-    int? year = int.tryParse(fieldTexts["year"]!);
-    int? month = int.tryParse(fieldTexts['month']!);
-    int? day = int.tryParse(fieldTexts['day']!);
-    bool isValidDate = false;
-
-    if (year != null && month != null && day != null) {
-      isValidDate = BirthPickerUtil.isValidDate(year, month, day);
-    }
-
-    return isValidDate ? DateTime(year!, month!, day!) : null;
-  }
-
   void _setSelectedDate(DateTime selectedDate) {
     controllers['year']!.text = selectedDate.year.toString().padLeft(4, '0');
     controllers['month']!.text = selectedDate.month.toString().padLeft(2, '0');
@@ -204,154 +401,6 @@ class _BirthPickerState extends State<BirthPicker> {
     fieldTexts['month'] = controllers['month']!.text;
     fieldTexts['day'] = controllers['day']!.text;
     setState(() {});
-  }
-
-  void _handleKeyEvent(KeyEvent event) {
-    // Get the currently focused field.
-    final focusedField = focusNodes.entries
-        .firstWhere((entry) => entry.value.hasFocus,
-            orElse: () => MapEntry('', FocusNode()))
-        .key;
-
-    if (focusedField.isEmpty) return;
-
-    final currentIndex = fieldOrder.indexOf(focusedField);
-
-    if (event.logicalKey == LogicalKeyboardKey.arrowLeft && currentIndex > 0) {
-      _requestFocus(fieldOrder[currentIndex - 1]);
-    } else if (event.logicalKey == LogicalKeyboardKey.arrowRight &&
-        currentIndex < fieldOrder.length - 1) {
-      _requestFocus(fieldOrder[currentIndex + 1]);
-    }
-  }
-
-  Widget _buildField(BuildContext context, String fieldType) {
-    final controller = controllers[fieldType]!;
-    final focusNode = focusNodes[fieldType]!;
-    final hintText = hintTexts[fieldType]!;
-    final maxLength = (fieldType == 'year') ? 4 : 2;
-    final defaultTextStyle = widget.textStyle ?? TextStyle(fontSize: 16);
-    final text = fieldTexts[fieldType]!;
-
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: () => _requestFocus(fieldType),
-      child: Stack(
-        children: [
-          Center(
-            child: Row(
-              children: [
-                Container(
-                  padding: widget.focusPadding ??
-                      const EdgeInsets.symmetric(horizontal: 2, vertical: 2),
-                  decoration: BoxDecoration(
-                    color: focusNode.hasFocus
-                        ? widget.focusColor ??
-                            (Theme.of(context).brightness == Brightness.dark
-                                ? const Color(0xff2176FF).withValues(alpha: 0.7)
-                                : const Color(0xffb4d7ff))
-                        : null,
-                  ),
-                  child: Text(
-                    text.isEmpty ? hintText : text.padLeft(maxLength, '0'),
-                    style: defaultTextStyle,
-                  ),
-                ),
-                Text(
-                  ".",
-                  style: defaultTextStyle,
-                ),
-              ],
-            ),
-          ),
-          IgnorePointer(
-            ignoring: true,
-            child: Center(
-              child: SizedBox(
-                width: 0,
-                height: 0,
-                child: TextField(
-                  controller: controller,
-                  focusNode: focusNode,
-                  keyboardType: TextInputType.number,
-                  enableInteractiveSelection: false,
-                  enableSuggestions: false,
-                  autofocus: widget.autofocus && fieldType == fieldOrder.first,
-                  decoration: const InputDecoration(
-                    isDense: true,
-                    counterText: '',
-                    border: UnderlineInputBorder(borderSide: BorderSide.none),
-                  ),
-                  onChanged: (value) {
-                    if (text.length > value.length) {
-                      fieldTexts[fieldType] = "";
-                      controller.clear();
-                    } else {
-                      if (value.length > maxLength) {
-                        value = value.substring(maxLength, value.length);
-                      }
-
-                      if (BirthPickerUtil.isInteger(value)) {
-                        // 숫자일 경우 실행할 코드
-                        var (correctedValue, moveNext) =
-                            _checkDateFormat(fieldType, value);
-
-                        fieldTexts[fieldType] = correctedValue;
-
-                        if (moveNext) {
-                          _moveFocusToNextField(fieldType);
-                        }
-                        controller.text = correctedValue;
-                      } else {
-                        controller.text = text;
-                      }
-                    }
-                    setState(() {});
-                  },
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  (String, bool) _checkDateFormat(String fieldType, String input) {
-    bool moveNext = false;
-    String correctedValue = input;
-    int number = int.parse(input);
-
-    if (fieldType == 'year') {
-      moveNext = input.length >= 4;
-      if (input == "0000") {
-        correctedValue = "0001";
-      }
-    } else if (fieldType == 'month') {
-      moveNext = input.length >= 2;
-
-      if (input == "00") {
-        correctedValue = "01";
-      } else if (number > 2 && number < 10) {
-        moveNext = true;
-        correctedValue = "0$number";
-      } else if (number > 12) {
-        correctedValue = "12";
-      }
-    } else if (fieldType == 'day') {
-      moveNext = input.length >= 2;
-
-      if (input == "00") {
-        correctedValue = "01";
-      } else if (number > 3 && number < 10) {
-        moveNext = true;
-        correctedValue = "0$number";
-      } else if (number > 31) {
-        correctedValue = "31";
-      }
-    }
-
-    return (correctedValue, moveNext);
   }
 
   void _moveFocusToNextField(String currentField) {
@@ -365,90 +414,6 @@ class _BirthPickerState extends State<BirthPicker> {
 
   void _requestFocus(String fieldType) {
     focusNodes[fieldType]!.requestFocus();
-  }
-
-  Map<String, String> getLocalizedDateUnits(String locale) {
-    List<String> dateFormat = BirthPickerLocalization.getDateFormat(locale);
-
-    return {
-      'year': dateFormat[0],
-      'month': dateFormat[1],
-      'day': dateFormat[2],
-    };
-  }
-
-  void _setFieldOrder() {
-    String? pattern =
-        DateFormat.yMd(widget.locale ?? Platform.localeName).pattern;
-    if (pattern != null) {
-      fieldOrder = _extractFieldOrder(pattern);
-    }
-  }
-
-  List<String> _extractFieldOrder(String pattern) {
-    List<MapEntry<String, int>> fieldsWithPosition = [
-      MapEntry('year', pattern.indexOf('y')),
-      MapEntry('month', pattern.indexOf('M')),
-      MapEntry('day', pattern.indexOf('d')),
-    ]..sort((a, b) => a.value.compareTo(b.value));
-
-    return fieldsWithPosition.map((entry) => entry.key).toList();
-  }
-
-  void _addFocusListeners() {
-    for (var entry in focusNodes.entries) {
-      entry.value.addListener(() {
-        String fieldType = entry.key;
-        String text = fieldTexts[fieldType]!;
-
-        // Automatically checks the format of text when focus is released
-        if (!entry.value.hasFocus && text.isNotEmpty) {
-          final maxLength = (fieldType == 'year') ? 4 : 2;
-          var (correctedValue, _) =
-              _checkDateFormat(fieldType, text.padLeft(maxLength, "0"));
-          fieldTexts[fieldType] = correctedValue;
-          controllers[fieldType]!.text = correctedValue;
-        }
-
-        setState(() {});
-      });
-    }
-  }
-
-  void _addControllerListeners() {
-    for (var controller in controllers.values) {
-      controller.addListener(() async {
-        // Keep the cursor always at the end of the text
-        if (controller.selection ==
-            TextSelection.fromPosition(
-                TextPosition(offset: controller.text.length - 1))) {
-          controller.selection = TextSelection.fromPosition(
-            TextPosition(
-              offset: controller.text.length,
-            ),
-          );
-        }
-
-        if (widget.onChanged != null) {
-          await Future.delayed(Duration.zero);
-          final date = _getDate();
-
-          if (currentDate != date) {
-            currentDate = date;
-            widget.onChanged!.call(date);
-          }
-        }
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    hintTexts = getLocalizedDateUnits(widget.locale ?? Platform.localeName);
-    _setFieldOrder();
-    _addFocusListeners();
-    _addControllerListeners();
   }
 
   @override
--- lib/birth_picker_util.dart
+++ lib/birth_picker_util.dart
@@ -8,8 +8,45 @@ abstract class BirthPickerUtil {
     try {
       DateTime date = DateTime(year, month, day);
       return date.year == year && date.month == month && date.day == day;
-    } catch (e) {
+    } catch (_) {
       return false;
     }
+  }
+
+  static (String, bool) checkDateFormat(String fieldType, String input) {
+    bool moveNext = false;
+    String correctedValue = input;
+    int number = int.parse(input);
+
+    if (fieldType == 'year') {
+      moveNext = input.length >= 4;
+      if (input == "0000") {
+        correctedValue = "0001";
+      }
+    } else if (fieldType == 'month') {
+      moveNext = input.length >= 2;
+
+      if (input == "00") {
+        correctedValue = "01";
+      } else if (number > 2 && number < 10) {
+        moveNext = true;
+        correctedValue = "0$number";
+      } else if (number > 12) {
+        correctedValue = "12";
+      }
+    } else if (fieldType == 'day') {
+      moveNext = input.length >= 2;
+
+      if (input == "00") {
+        correctedValue = "01";
+      } else if (number > 3 && number < 10) {
+        moveNext = true;
+        correctedValue = "0$number";
+      } else if (number > 31) {
+        correctedValue = "31";
+      }
+    }
+
+    return (correctedValue, moveNext);
   }
 }
